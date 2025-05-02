a0b25097da5ec1a47a574280ec4b9777e0dcf2ca
YuhichYOC <you.65535.kir@gmail.com>
2025-05-02 18:29:22 +0900

    Apply missing fixes.

--- browser/settings.py
+++ browser/settings.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
-import os
+import os.path
 from pathlib import Path
+
+import environ
+env = environ.Env()
+env.read_env('.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%&jg#vw6jk10y+iq3fb9swkly!u8m7ooyuf29cb&4kq6m09mq='
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS')
 
 # Application definition
 
@@ -38,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'browser',
     'logicaldoc',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
